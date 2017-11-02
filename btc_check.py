3ca9f6879d2369c57c3d13f5678ee1a93c475865
TISCOGROUP\TawatchaiP <tawatchaip@tisco.co.th>
2017-11-02 12:50:05 +0700

    btc check code

--- btc_check.py
+++ btc_check.py
@@ -1,10 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+import time
+import requests
+import json
+
+count = 0
+
+def line_notification(alert_message):
+    headers = {'authorization': 'Bearer kHAuKPObW0Bc2kt2ltt1TEQhgbsXmfkAL1LR97OOUXh'}
+    url = 'https://notify-api.line.me/api/notify'
+    data = {'message': alert_message}
+    r = requests.post(url, headers=headers, data=data)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+def executeSomething():
+    global count
 
-if __name__ == '__main__':
-    app.run()
+    #code here
+    count = count + 1
+    print("Executing for : ", count)
+    response = requests.get("https://bx.in.th/api/")
+    json_data = json.loads(response.text)
+    # print("response is ",json_data)
+    # print("type of json data is : ", type(json_data))
+    current_btc_price = json_data["1"]["last_price"]
+
+
+    if(current_btc_price>229000):
+        line_notification("HIGHER THAN 229000")
+    elif(current_btc_price<221000):
+        line_notification("LOWER THAN 221000")
+    else:
+        print("not aleart")
+
+    time.sleep(5)
+
+while True:
+    executeSomething()
